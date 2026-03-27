2b8a600cab5db0f83ae2dc7a60b17f33022f5b33
Tuan-Minh-0402 <124855504+Tuan-Minh-0402@users.noreply.github.com>
2026-03-27 17:15:32 +0700

    Delete New folder directory

--- New folder/app/src/main/java/com/example/personaltaskapp/model/PomodoroState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.personaltaskapp.model
-
-enum class PomodoroState {
-    WORK,
-    SHORT_BREAK,
-    LONG_BREAK,
-    IDLE
-}
--- New folder/app/src/main/java/com/example/personaltaskapp/model/TaskSort.kt
+++
@@ -1,6 +0,0 @@
-package com.example.personaltaskapp.model
-
-enum class TaskSort(val displayName: String) {
-    TITLE_ASC("Title Ascending"),
-    TITLE_DESC("Title Descending")
-}
